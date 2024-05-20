242425b72fcbd8e14b2fcfbdcc862016f71b4b37
deshinta <deshintaaulia065@gmail.com>
2024-05-20 19:36:13 +0700

    feat: add project

---
+++ src/main/java/com/domain/models/entities/FinancialAdvisor.java
@@ -0,0 +1,9 @@
+package com.domain.models.entities;
+
+import java.io.Serializable;
+
+// @Entity
+// @Table(name="tbl_FinancialAdvisor")
+public class FinancialAdvisor implements Serializable {
+    
+}
